d65427284ef05587012aceccd935c7c78322b459
Kurnia Muhamad <omenkzz@gmail.com>
2025-02-26 23:30:40 +0700

    Optimize cart loading by eager loading product images

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
-  
   def show
     @order = current_order || Order.new
-    @order_items = @order.order_items
+    @order_items = @order.order_items.includes(product_variant: [ product: [images_attachments: :blob]])
   end
+
 end
